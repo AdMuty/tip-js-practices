ebc806d1ef8c7fe3a697b7d48be454fffccbb18e
Трушков Андрей <trushkovandrey56@gmail.com>
2026-09-15 13:43:05 +0300

    feat: explore types and conversions

---
+++ practice-01/js/types.js
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log('"8" + 2 =', "8" + 2, '| тип:', typeof ("8" + 2));
+console.log('"8" - 2 =', "8" - 2, '| тип:', typeof ("8" - 2));
+console.log('Number("8") + 2 =', Number("8") + 2, '| тип:', typeof (Number("8") + 2));
